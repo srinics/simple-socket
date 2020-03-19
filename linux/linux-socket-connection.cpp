dde1b2d0551731761561901f0b0cfec5b94a042c
Srini R <srinics@hotmail.com>
2020-03-19 15:11:45 +0000

    Add linux socket conn file

---
+++ linux/linux-socket-connection.cpp
@@ -0,0 +1,47 @@
+/*
+Author: M1043833 ( Srinivasan Rajendran )
+Description: This file contains function for getting Windows Create TCP/UDP Socket ( CreateSocket ) and
+function for closing the socket connection
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdexcept>
+#include "socket-connection.h"
+
+using namespace std;
+
+
+/*
+Author: M1043833
+Function Name: CreateSocket
+Description:
+Function used for creating the socket and it is wrapper function for socket api and it wil take three
+arguements family either AF_INET/PF_INET, stream SOCK_STREAM/SOCK_DGRAM and protocol if any. And default values
+for family, stream, protocol are AF_INET, SOCK_STREAM, 0 respectively
+*/
+int CreateSocket(int family = AF_INET, int stream = SOCK_STREAM, int protocol = 0){
+	int sd = socket(family, stream, protocol);
+	if (sd < 0)
+	{
+		throw std::runtime_error("Socket Creation Failed");
+	}
+
+	std::cout << "Socket created Successfully..." << std::endl;
+	return sd;
+}
+
+
+/*
+Author: M1043833
+Function Name: CloseSocket
+Description:
+Function used for closing socket.
+*/
+void CloseSocket(int sd){
+	if (sd < 0){
+		throw std::exception("Invalid Socket SD");
+	}
+	close(sd);
+}
+
